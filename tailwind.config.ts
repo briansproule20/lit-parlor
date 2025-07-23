488b01a86236ad4c3d31942df6624883498d877a
Brian Sproule <briansproule20@gmail.com>
2025-07-23 19:09:21 -0400

    Fix current challenges carousel on deployment by adding marquee animations to safelist

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,10 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: [
+    'animate-marquee',
+    'animate-marquee-vertical',
+  ],
   theme: {
     extend: {
       backgroundImage: {
